7403f5cc8663facdebf624315b0220a5a7597382
Vadim Yakhin <yakhin.v@gmail.com>
2016-09-04 21:54:27 +0300

    add composeDictionaryEntry from old longman-to-anki

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fetch = require('node-fetch');
-const cheerio = require('cheerio');
+const composeDictionaryEntry = require('./utils/composeDictionaryEntry.js')
 
 fetch('http://www.ldoceonline.com/dictionary/deliberately')
   .then(res => res.text())
   .then(body => {
-    let $ = cheerio.load(body);
-    console.log($('.HWD').text());
+    const entry = composeDictionaryEntry(body);
+    console.log(entry);
   })
+  .catch(err => console.log(err));
