8f78a2f7857c2ec7d493fa7badfde0cd023618ef
Vadim Yakhin <yakhin.v@gmail.com>
2016-09-05 20:37:54 +0300

    add feature: write result to file

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const fetch = require('node-fetch');
 const composeDictionaryEntry = require('./utils/composeDictionaryEntry.js');
 const urls = require('./urls.json');
+const fs = require('fs');
 
 Promise.all(
   urls.map(url => fetch(url)
     .then(res => res.text())
   )
 )
-  .then(bodies => bodies.forEach(
-    body => console.log(composeDictionaryEntry(body))
+  .then(bodies => bodies.map(composeDictionaryEntry))
+  .then(entries => entries.reduce(
+    (body, acc) => `${acc}${body}`
   ))
+  .then(result => fs.writeFileSync('result.txt', result))
   .catch(err => console.log(err));
